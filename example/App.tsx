22428cbd8dc2209089c4a5bed55c63509ebf13fb
spike04 <rubinbajracharya@yahoo.com>
2024-09-08 14:40:33 +0545

    learning expo module getTheme

--- example/App.tsx
+++ example/App.tsx
@@ -1,11 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native';
-
-import * as ExpoSettings from 'expo-settings';
+import * as Settings from "expo-settings";
+import { useEffect, useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [theme, setTheme] = useState<string>(Settings.getTheme());
+  const nextTheme = theme === "dark" ? "light" : "dark";
+
+  useEffect(() => {
+    const subscription = Settings.addThemeListener(({ theme }) => {
+      setTheme(theme);
+    });
+
+    return () => {
+      subscription.remove();
+    };
+  }, [setTheme]);
+
   return (
-    <View style={styles.container}>
-      <Text>{ExpoSettings.hello()}</Text>
+    <View
+      style={[
+        styles.container,
+        {
+          backgroundColor: theme === "dark" ? "black" : "white",
+        },
+      ]}
+    >
+      <Text style={{ color: theme === "dark" ? "white" : "black" }}>
+        Theme: {theme}
+      </Text>
+
+      <Button
+        title="Change theme"
+        onPress={() => Settings.setTheme(nextTheme)}
+      />
     </View>
   );
 }
@@ -13,8 +40,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
